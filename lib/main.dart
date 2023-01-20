ea8bdbfde95a8b9eb1ff186a06ac6735c4586fab
br-ai <brandonsitedjeya@.com>
2023-01-20 22:38:17 +0100

    textbuton and iconbuton

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.orange,
       ),
       home: soir(),
     );
@@ -30,31 +30,67 @@ class soir extends StatefulWidget {
 
 // ignore: camel_case_types
 class _soirState extends State<soir> {
-  var info = 'Floatting action button';
-  String value = '';
-
-  void onshow() => setState(() {
-        value = new DateTime.now().toString();
-      });
+  var info = 'les buttons';
+  int note = 0;
+  @override
+  void initState() {
+    super.initState();
+    info = 'Bienvenue sur la page';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(info),
-      ),
-      body: Center(
-        child: Text(
-          value,
-          style: TextStyle(color: Colors.pink),
+        appBar: AppBar(
+          title: Text(info),
         ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => onshow(),
-        backgroundColor: Colors.red,
-        label: Text('time'),
-        icon: Icon(Icons.timer),
-      ),
-    );
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(
+              onPressed: () => debugPrint('Okay'),
+              child: Text(
+                'Boutton flat',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  note += 1;
+                });
+              },
+              icon: Icon(
+                Icons.thumb_up,
+                color: Colors.blue,
+                size: 70,
+              ),
+              tooltip: 'jaime',
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  note -= 1;
+                });
+              },
+              icon: Icon(
+                Icons.thumb_down,
+                color: Colors.red,
+                size: 70,
+              ),
+              tooltip: 'je n aime pas',
+            ),
+            Text(
+              'Votre notre de point est $note',
+              style: TextStyle(color: Colors.indigo, fontSize: 20),
+            )
+          ],
+        ));
   }
 }
